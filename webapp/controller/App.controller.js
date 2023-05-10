679103c528cae06ec218f9697319613637d65f20
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-05-10 13:52:22 +0100

    json model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
- ], (Controller, MessageToast) => {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello :  () => {
-            // JS alert("Hello World")
-            // alert("Alert from app controller");
-            MessageToast.show("Alert from app controller");
-         }
-    });
- });
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel",
+], (Controller, MessageToast, JSONModel) => {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit : function () {
+         // set data model on view
+         var oData = {
+            recipient : {
+               name : "UI5 User"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+      },
+      onShowHello: () => {
+         // JS alert("Hello World")
+         // alert("Alert from app controller");
+         MessageToast.show("Alert from app controller");
+      }
+   });
+});
