883f8e9224920b39f5c4919a50f6885f3e7c7b1b
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-05-10 17:52:58 +0100

    add ComponentContainer

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView",
-], (XMLView) => {
+	"sap/ui/core/ComponentContainer",
+], (ComponentContainer) => {
 	"use strict";
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
-	}).then(
-		(oView) => { oView.placeAt("content"); }
-	);
-	alert("UI5 is ready");
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings: {
+			id: "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
